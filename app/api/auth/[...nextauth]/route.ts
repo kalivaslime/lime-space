580890e36dcfcb420ac6c0ea74c563b180b6192b
Christos Kalivas <chrkalivas@gmail.com>
2023-05-06 00:35:30 +0300

    prisma setup, nextAuth schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
+import {prisma} from '@/lib/prisma'
+import {PrismaAdapter} from '@next-auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import type {NextAuthOptions} from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -11,5 +15,17 @@ export const authOptions: NextAuthOptions = {
   ],
 }
 
-const handler = NextAuth(authOptions)
+export const handler = NextAuth(authOptions)
 export {handler as GET, handler as POST}
+
+// export async function getServerSideProps(context) {
+//   return {
+//   props: {
+//   session: await getServerSession(
+//   context.req,
+//   context.res,
+//   authOptions
+//   ),
+//   },
+//   }
+//   }
